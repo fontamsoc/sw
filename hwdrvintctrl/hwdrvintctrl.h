4415d0739d5e11ddd96336b5f795e78e131c8d1e
William Tambe <tambewilliam@gmail.com>
2021-10-16 15:40:35 -0500

    support for newer hardware interrupt controller implementation

--- hwdrvintctrl/hwdrvintctrl.h
+++ hwdrvintctrl/hwdrvintctrl.h
@@ -5,25 +5,31 @@
 #define HWDRVINTCTRL_H
 
 // PIRWOP is the only memory operation handled with following
-// expected from pi1_data_i |arg: (ARCHBITSZ-1) bits|cmd: 1 bit|
-// where the field "cmd" values are CMDACKINT(0) or CMDINTDST(1):
-// 	CMDACKINT: Acknowledges an interrupt source; pi1_data_o get set
-// 	to the interrupt source index or -1 (for an interrupt triggered
-// 	by CMDINTDST), while the field "arg" from pi1_data_i is ignored.
+// expected from pi1_data_i |arg: (ARCHBITSZ-2) bits|cmd: 2 bit|
+// where the field "cmd" values are CMDACKINT(2'b00), CMDINTDST(2'b01) or CMDENAINT(2'b10).
+// 	CMDACKINT: Acknowledges an interrupt source; field "arg" is expected
+// 	to have following format |idx: (ARCHBITSZ-3) bits|en: 1 bit|
+// 	where "idx" is the interrupt destination index, "en" enable/disable
+// 	further interrupt delivery to the interrupt destination "idx".
+// 	pi1_data_o get set to the interrupt source index, or -2 if
+// 	there are no pending interrupt for the destination "idx",
+// 	or -1 (for an interrupt triggered by CMDINTDST).
 // 	CMDINTDST: Triggers an interrupt targeting a specific destination;
 // 	the field "arg" from pi1_data_i is the index of the interrupt destination
 // 	to target, while pi1_data_o get set to the interrupt destination index
-// 	if valid or -1 if invalid.
+// 	if valid, -2 if not ready due to an interrupt pending ack, or -1 if invalid.
+// 	CMDENAINT: Enable/Disable an interrupt source; field "arg" is expected
+// 	to have following format |idx: (ARCHBITSZ-3) bits|en: 1 bit|
+// 	where "idx" is the interrupt source index, "en" enable/disable
+// 	interrupts from the interrupt source "idx".
+// 	pi1_data_o get set to the interrupt source index, or -1 if invalid.
 // An interrupt must be acknowledged as soon as possible so
 // that the intctrl can dispatch another interrupt request.
-// After reset, only the interrupt destination 0 receives interrupts
-// until CMDINTDST(1) update the highest interrupt destination index
-// if a higher index is used.
 
 #define INTCTRLADDR ((void *)0x0ff0 /* By convention, the interrupt controller is located at 0x0ff0 */)
 
-static unsigned long hwdrvintctrl_ack (void) {
-	unsigned long intsrc = 0;
+static inline unsigned long hwdrvintctrl_ack (unsigned long idx, unsigned long en) {
+	unsigned long intsrc = ((idx<<3) | ((en&1)<<2) | 0b00);
 	__asm__ __volatile__ (
 		"ldst %0, %1"
 		: "+r" (intsrc)
@@ -31,13 +37,22 @@ static unsigned long hwdrvintctrl_ack (void) {
 	return intsrc;
 }
 
-static unsigned long hwdrvintctrl_int (unsigned long idx) {
-	unsigned long intdst = ((idx<<1) | 1);
+static inline unsigned long hwdrvintctrl_int (unsigned long idx) {
+	unsigned long intdst = ((idx<<2) | 0b01);
 	__asm__ __volatile__ (
 		"ldst %0, %1"
 		: "+r" (intdst)
 		: "r" (INTCTRLADDR));
 	return intdst;
+}
+
+static inline unsigned long hwdrvintctrl_ena (unsigned long idx, unsigned long en) {
+	unsigned long intsrc = ((idx<<3) | ((en&1)<<2) | 0b10);
+	__asm__ __volatile__ (
+		"ldst %0, %1"
+		: "+r" (intsrc)
+		: "r" (INTCTRLADDR));
+	return intsrc;
 }
 
 #endif /* HWDRVINTCTRL_H */
